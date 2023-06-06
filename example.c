f025833037bd87a8a8183ec08937781fef93aa49
Henry Wilder <henrythepony@gmail.com>
2023-06-05 20:59:57 -0400

    Added to example

--- example.c
+++ example.c
@@ -1,7 +1,26 @@
+#include <stdio.h>
 #include "solitaire-sort.h"
 
 int main()
 {
-    card_t data[5];
-    SolitaireSort(data, sizeof(data) / sizeof(card_t));
+    card_t data[] = {
+        '1',
+        '5',
+        '2',
+        '5',
+        '3',
+        '9',
+        '6',
+        '9',
+        '7',
+        '0',
+        '4',
+    };
+    const int numItems = sizeof(data) / sizeof(card_t);
+    int success = SolitaireSort(data, numItems);
+    for (int i = 0; i < numItems; ++i)
+    {
+        printf("%c, ", data[i]);
+    }
+    return success;
 }
