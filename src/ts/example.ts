24974f292597419e3e0c1cbcbcb81c55afc09af3
HenryWilder <henrythepony@gmail.com>
2023-06-07 10:05:28 -0400

    Made cards monospace

--- src/ts/example.ts
+++ src/ts/example.ts
@@ -2,6 +2,6 @@ import { solitaireSort } from "./solitaire-sort";
 
 const data: string[] = [];
 for (let i = 0; i < 52; ++i) {
-    data.push(`${Math.floor(Math.random() * 13) + 1}`)
+    data.push("A234567890JQK"[Math.floor(Math.random() * 13)])
 }
 solitaireSort(data);
