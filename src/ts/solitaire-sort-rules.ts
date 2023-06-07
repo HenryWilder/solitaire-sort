3fd6d411606dabfc7c8751db646b968ef380d1df
Henry Wilder <henrythepony@gmail.com>
2023-06-06 22:00:41 -0400

    Added hand

---
+++ src/ts/solitaire-sort-rules.ts
@@ -0,0 +1,22 @@
+/**
+ * @file Provides the ruleset constants for solitaire-sort.ts.
+ */
+
+/**
+ * Namespace for Solitaire Sort rules
+ */
+export namespace rules {
+
+    /**
+     * Whether random access is allowed for `Hand`.
+     * @constant
+     */
+    export const HAND_ALLOW_RANDOM_ACCESS: boolean = true;
+
+    /**
+     * The maximum number of elements allowed in a `Hand`.
+     * @constant
+     */
+    export const HAND_SIZE_MAX: number = 3;
+
+}
--- src/ts/solitaire-sort.ts
+++ src/ts/solitaire-sort.ts
@@ -10,12 +10,21 @@
  * @remark This project is explicitly a joke and not meant for production.
  */
 
+import { rules } from "./solitaire-sort-rules";
+
+/**
+ * A "card".
+ * Meant to be equivalent to a `char` in C.
+ */
 type Card = string;
 
 /**
  * A stack of cards.
+ * 
+ * @summary A stack.
  */
 class CardStack {
+
     public constructor(
         /**
          * The list of cards in the stack.
@@ -26,12 +35,14 @@ class CardStack {
          * The number of cards considered public, counts starting from the top (the back/last element)
          */
         public faceUp: number,
-    ) { }
+    ) {
+        console.assert(this.faceUp <= this.numCards);
+    }
 
     /**
      * Tells how many **total** cards are in the stack - both `faceUp` and `faceDown`.
      */
-    public get size(): number {
+    public get numCards(): number {
         return this.cards.length;
     }
 
@@ -39,14 +50,14 @@ class CardStack {
      * Tells how many `faceDown` cards are in the stack - only cards that are not `faceUp`.
      */
     public get faceDown(): number {
-        return this.size - this.faceUp;
+        return this.numCards - this.faceUp;
     }
 
     /**
      * Gives a readable list of all `faceUp` cards from the top of the stack.
      */
     public get faceUpCards(): Card[] {
-        return this.cards.slice(this.size - this.faceUp);
+        return this.cards.slice(this.numCards - this.faceUp);
     }
 
     /**
@@ -69,8 +80,8 @@ class CardStack {
         if (n === 0) {
             return [];
         }
-        console.assert(this.size >= n);
-        console.assert(this.faceUp >= n);
+        console.assert(n <= this.numCards);
+        console.assert(n <= this.faceUp);
         const result: Card[] = this.cards.slice(-n);
         this.cards.length -= n;
         return result;
@@ -80,30 +91,139 @@ class CardStack {
      * Makes `n`-**more** cards `faceUp`.
      */
     public reveal(n: number): void {
+        console.assert((this.faceUp + n) <= this.numCards);
         this.faceUp += n;
     }
-
 
     /**
      * Makes `n`-**fewer** cards `faceUp`.
      */
     public conceal(n: number): void {
-        console.assert(this.faceUp >= n);
+        console.assert(n <= this.faceUp);
         this.faceUp -= n;
     }
 }
 
 /**
- * A CardStack that can have cards pushed to the bottom of the stack.
+ * A queue of cards that can have cards pushed to the bottom and pulled from the top.
+ * Cards in the deck cannot be faceup, and all are facedown.
+ * It is expected that the Deck only has its cards interacted with via the {@linkcode Hand}.
+ * 
+ * @summary A queue.
  */
-class Deck extends CardStack {
+class Deck {
+
+    public constructor(
+        /**
+         * The list of cards in the stack.
+         * The back (last element) is called the top, while the front (first element) is called the bottom.
+         */
+        protected cards: Card[],
+    ) { }
+
+    get numCards(): number {
+        return this.cards.length;
+    }
 
     /**
-     * Slides `cards` underneath the stack. The new cards will **not** be `faceUp`.\
-     * Existing `faceUp` cards on top of the stack will *remain* `faceUp`.
+     * Slides `cards` underneath the stack.
+     * @param cards The cards to push to the front of the queue.
      */
     public pushToBottom(cards: Card[]): void {
         this.cards = cards.concat(this.cards);
+    }
+
+    /**
+     * ## Only {@linkcode Hand} is intended use this.
+     * ### It's not like anything is gonna break, it's just the rules of the game.
+     * Removes and returns the requested number of cards from the queue.
+     * @param n The number of cards to pull from the back of the queue.
+     */
+    public pullFromTop(n: number): Card[] {
+        if (n === 0) {
+            return [];
+        }
+        console.assert(n <= this.numCards);
+        const result: Card[] = this.cards.slice(-n);
+        this.cards.length -= n;
+        return result;
+    }
+}
+
+/**
+ * A bounded card stack that has a maximum capacity (of {@linkcode rules.HAND_SIZE_MAX|HAND_SIZE_MAX}).
+ * Only allows pulling one element at a time.
+ * Random access is allowed when {@linkcode rules.HAND_ALLOW_RANDOM_ACCESS|HAND_ALLOW_RANDOM_ACCESS} is `true`.
+ * 
+ * Cards cannot be pushed individually, and can only be given in the form of a `Deck`.
+ * 
+ * @summary A fixed-capacity vector.
+ */
+class Hand {
+    public constructor(
+        /**
+         * The list of cards in the stack.
+         * The back (last element) is called the top, while the front (first element) is called the bottom.
+         */
+        protected cards: Card[],
+    ) {
+        console.assert(cards.length <= rules.HAND_SIZE_MAX);
+    }
+
+    /**
+     * The number of cards currently in the hand.
+     * [0..{@linkcode rules.HAND_SIZE_MAX|HAND_SIZE_MAX})
+     */
+    public get numCards(): number {
+        return this.cards.length;
+    }
+
+    /**
+     * Removes and returns the card at the provided index.  
+     * Do not use if `cards` is empty.
+     * @returns `null` if {@linkcode rules.HAND_ALLOW_RANDOM_ACCESS|HAND_ALLOW_RANDOM_ACCESS} is false.
+     * @param index The zero-based index of the card to pull.
+     */
+    public pullAt(index: number): Card | null {
+
+        if (!rules.HAND_ALLOW_RANDOM_ACCESS) {
+            console.error("Random Hand access is currently disallowed by the HAND_ALLOW_RANDOM_ACCESS rule.");
+            return null;
+        }
+
+        console.assert(this.numCards > 0);
+        console.assert(index < this.numCards);
+
+        // ? I'm curious if it would be better to just use splice
+
+        if (index === 0) {
+            return this.cards.shift()!;
+        } else if (index === (this.numCards - 1)) {
+            return this.cards.pop()!;
+        } else {
+            return this.cards.splice(index, 1)![0];
+        }
+    }
+
+    /**
+     * Removes and returns the card at the top.
+     * @returns The card at the top.
+     */
+    public pull(): Card {
+        console.assert(this.numCards > 0);
+        return this.cards.pop()!;
+    }
+
+    /**
+     * Draws a new set of cards from the deck.
+     * Passes the full contents of the hand to the bottom of the deck, then pulls
+     * {@linkcode rules.HAND_ALLOW_RANDOM_ACCESS|HAND_ALLOW_RANDOM_ACCESS} cards from the top of the deck to insert back into the hand.
+     * If the deck has fewer than {@linkcode rules.HAND_SIZE_MAX|HAND_SIZE_MAX} cards,
+     * the entire remaining deck will be emptied into the hand.
+     */
+    public draw(deck: Deck): void {
+        deck.pushToBottom(this.cards);
+        this.cards = deck.pullFromTop(Math.min(rules.HAND_SIZE_MAX, deck.numCards));
     }
 }
 
@@ -118,7 +238,7 @@ const transfer = (dest: CardStack | Deck, src: CardStack | Deck, n: number): voi
         return;
     }
 
-    console.assert(n <= src.size);
+    console.assert(n <= src.numCards);
     console.assert(n <= src.faceUp);
 
     dest.pushToTop(src.pullFromTop(n));
